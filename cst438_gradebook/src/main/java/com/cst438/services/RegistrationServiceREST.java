80f173ac49124808b2e6ad7ca73cba21d6fb3325
RyRom <109193666+RyRom@users.noreply.github.com>
2023-10-10 18:34:22 -0700

    First attempt at sending message

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -3,6 +3,7 @@ package com.cst438.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,17 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.cst438.domain.FinalGradeDTO;
-import com.cst438.domain.Course;
 import com.cst438.domain.CourseRepository;
 import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.EnrollmentRepository;
-import com.cst438.domain.Enrollment;
 
 @Service
 @ConditionalOnProperty(prefix = "registration", name = "service", havingValue = "rest")
 @RestController
 public class RegistrationServiceREST implements RegistrationService {
-
+	
 	
 	RestTemplate restTemplate = new RestTemplate();
 	
@@ -35,7 +34,21 @@ public class RegistrationServiceREST implements RegistrationService {
 	@Override
 	public void sendFinalGrades(int course_id , FinalGradeDTO[] grades) { 
 		
-		//TODO use restTemplate to send final grades to registration service
+		ResponseEntity<FinalGradeDTO> response = restTemplate.postForEntity(
+							"/course/{course_id}/finalgrades",
+							grades,
+							FinalGradeDTO.class);
+		
+		if (response.getStatusCodeValue() == 200) {
+			// update database
+			FinalGradeDTO finalGrades = response.getBody();
+			restTemplate.put("http://localhost:8080/course", finalGrades);		
+		} else {
+			// error.
+			System.out.println(
+                         "Error: unable to post multiply_level "+
+                          response.getStatusCodeValue());
+		}
 		
 	}
 	
